700ac5872ead5cf3d34e154a3aca55ba3e7af6b8
Ndanema98 <davidnda26@yahoo.co.uk>
2022-10-02 21:06:32 +0000

    Feat:Add models.py for animes app

--- animes/models.py
+++ animes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    item_content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='anime_likes', blank=True)
+
+
+def __str__(self):
+    return self.item_name
+
+
+def number_of_likes(self):
+    return self.likes.count()
