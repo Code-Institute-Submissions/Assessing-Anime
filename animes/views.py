9778cc5603494400a385908128f6f98c9922864e
Ndanema98 <davidnda26@yahoo.co.uk>
2022-10-03 13:48:54 +0000

    Feat: Add context to index.html

--- animes/views.py
+++ animes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello')
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
 
+    return render(request, 'animes/index.html', context)
 
 def item(request):
     return HttpResponse('Item view')
