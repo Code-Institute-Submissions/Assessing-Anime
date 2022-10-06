49055d1959bb38eb2a14e039bb79afa0907b21e3
Ndanema98 <davidnda26@yahoo.co.uk>
2022-10-06 09:26:11 +0000

    Feat: Link static files to html

--- animes/urls.py
+++ animes/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'animes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:item_id>',views.review,name='review'),
+    path('<int:item_id>', views.review, name='review'),
     path('item/', views.item, name='item'),
 ]
--- animes/views.py
+++ animes/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 from .models import Item
 from django.template import loader
 
 # Create your views here.
 
+
+class ItemList(generic.ListView):
+    model = Item
+    queryset = Item.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 6
 
 def index(request):
     item_list = Item.objects.all()
