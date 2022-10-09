0f783f8c2af26df173e4aace7f19e6c0231ce67f
Ndanema98 <davidnda26@yahoo.co.uk>
2022-10-09 00:48:21 +0000

    Feat: Add update functionality

--- animes/urls.py
+++ animes/urls.py
@@ -7,9 +7,13 @@ urlpatterns = [
     path('', views.ItemList.as_view(), name='ItemList'),
     #/animes/1
     path('item/', views.item, name='item'),
-    #add items
+    #add anime
     path('add', views.create_item, name='create_item'),
     #view likes
     path('like/<slug:slug>', views.AnimeLikes.as_view(), name='anime_likes'),
+    #review page
     path('<int:item_id>/', views.review, name='review'),
+    #edit anime
+    path('update/<int:id>/', views.update_item, name='update_item'),
+
 ]
--- animes/views.py
+++ animes/views.py
@@ -52,3 +52,15 @@ def create_item(request):
 
     return render(request, 'item-form.html', {'form': form})
 
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('animes:ItemList')
+
+    return render(request, 'item-form.html', {'form': form, 'item': item})
+    
+
+
